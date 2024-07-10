5a6dc548df4bbea5a969db1960bc02c6c1e91c1f
Song He <1311471185@qq.com>
2024-07-10 17:48:48 +0800

    init

---
+++ src/main/java/com/faken/apifox_test/ApifoxTestApplication.java
@@ -0,0 +1,13 @@
+package com.faken.apifox_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifoxTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApifoxTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/faken/apifox_test/ApifoxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faken.apifox_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifoxTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

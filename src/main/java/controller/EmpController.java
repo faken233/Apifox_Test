fe3d5d70caaa3ca5bf1a38ce6bea025f3cfa948e
Song He <1311471185@qq.com>
2024-07-11 09:06:37 +0800

    add test method "app"

---
+++ src/main/java/controller/EmpController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class EmpController {
+
+    @GetMapping("/app")
+    public String app() {
+        return "Hello World";
+    }
+
+}
